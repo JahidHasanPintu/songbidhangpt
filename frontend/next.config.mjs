8d1224fc7cf9c59d4746ec6f0dace06a3d88e981
Md Jahid Hasan Pintu <jahidhasanpintu82@gmail.com>
2026-04-02 11:18:16 +0600

    build error fixed

---
+++ frontend/next.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.agentsandcarriers.com",
+        pathname: "/img/**",
+      },
+      {
+        protocol: "https",
+        hostname: "python-api-ai-dev.s3.us-east-1.amazonaws.com",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
